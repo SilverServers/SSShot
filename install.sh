246f972b6d6099b982b0f14df087d6aee203af50
Austen Holland <austen.holland@silverservers.com>
2018-09-13 15:05:42 -0700

    Reverse installer messages

--- install.sh
+++ install.sh
@@ -9,5 +9,6 @@ NC='\e[0m'
 sudo scp ${DIR}/ssshot /usr/bin/
 sudo scp -r ${DIR}/SSShot /usr/bin/
 
-echo -e "[${GREEN}OK${NC}] SSShot is now installed! Give it a try with 'ssshot -h'";
+
 echo -e "[${YELLOW}INFO${NC}] If you want to enable features like remote server uploading, please edit the config at '/usr/bin/SSShot/ssshot.config'";
+echo -e "[${GREEN}OK${NC}] SSShot is now installed! Give it a try with 'ssshot -h'";
