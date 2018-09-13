c00064ae8a2ad12b831445e949b21ded0c4be2d7
Austen Holland <austen.holland@silverservers.com>
2018-09-13 14:56:56 -0700

    Update installer messages.

--- install.sh
+++ install.sh
@@ -3,9 +3,11 @@
 
 DIR=$(pwd)
 GREEN='\e[1;32m'
+YELLOW='\e[1;33m'
 NC='\e[0m'
 
 sudo scp ${DIR}/ssshot /usr/bin/
 sudo scp -r ${DIR}/SSShot /usr/bin/
 
-echo -e "[${GREEN}OK${NC}] SSShot is now installed! Please edit the config at '/usr/bin/SSShot/ssshot.config', then give it a try with 'ssshot -h'";
+echo -e "[${GREEN}OK${NC}] SSShot is now installed! Give it a try with 'ssshot -h'";
+echo -e "[${YELLOW}INFO${NC}] If you want to enable features like remote server uploading, please edit the config at '/usr/bin/SSShot/ssshot.config'";
