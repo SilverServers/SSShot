60652a3cf719e0a0fdcf599667ffa2d50bf171aa
Austen Holland <austen.holland@silverservers.com>
2018-09-18 14:00:02 -0700

    Remove dev file

--- dev.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-mapfile -t displays < <(xrandr | grep ' connected')
-get_date='`date +"%Y%m%d-%H%M%S"`'
-for (( i=0; i<${#displays[@]}; i++)); do
-  name=`echo ${displays[i]} | cut -d " " -f 1`
-  crop=`echo ${displays[i]} | cut -d " " -f 3`
-  echo import -silent -window root -crop ${crop} \"${name}-${get_date}.png\"
-done
